bf29c3e3be3e248ed78661a5140f0334c21fd1c7
Augusto Calaca <augustocalaca2@gmail.com>
2021-02-17 15:37:46 -0300

    add new folder structure

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 
-const Page = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  box-sizing: inherit;
-  background-color: #231940;
-`;
+import LandingPage from "./Pages/LandingPage";
 
-const Header = styled.div`
-  background-color: #e03b5d;
-  color: #ffffff;
-  border-radius: 8px;
-  font-size: 32px;
-  font-family: "Montserrat", sans-serif;
-  padding: 16px;
-`;
-
-const App = () => (
-  <Page>
-    <Header>Seja bem vindo ao EnfaseLabs Academy!</Header>
-  </Page>
-);
+const App = () => <LandingPage />;
 
 export default App;
---
+++ app/src/Components/WelcomeText.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.div`
+  background-color: #e03b5d;
+  color: #ffffff;
+  border-radius: 8px;
+  font-size: 32px;
+  font-family: "Montserrat", sans-serif;
+  padding: 16px;
+`;
+
+const WelcomeText = () => <Title>Seja bem vindo ao EnfaseLabs Academy!</Title>;
+
+export default WelcomeText;
