2d8df12a82281a91622fae032e4acc75ade4dd46
Augusto Calaca <augustocalaca2@gmail.com>
2021-02-17 15:56:24 -0300

    fix webpack entry ponti

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.jsx"),
+  entry: path.resolve(__dirname, "./src/index.js"),
   output: {
     path: path.resolve(__dirname + "/build"),
     filename: "index.bundle.js",
